ffb188ce656607f6b77d0856cb4a7f9dee66c0c1
origin <https://github.com/PMaruthiPrasad/test.git>
2023-08-25 06:33:20 +0530

    data ingestion notebook added

---
+++ src/textSymmarizer/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,15 @@
+from textSymmarizer.config.configuration import ConfigurationManager
+from textSymmarizer.components.data_ingestion import DataIngestion
+from textSymmarizer.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
